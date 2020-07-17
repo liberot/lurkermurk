e6b962e1646781bb9898bd333ff0e87fbfc4e9b3
Antoine Off <off@Antoines-MBP.fritz.box>
2020-07-18 00:49:48 +0200

    doc

--- Snoggie/Lurkemurk/view/frontend/web/catalog.js
+++ Snoggie/Lurkemurk/view/frontend/web/catalog.js
@@ -9,7 +9,10 @@ function(jQuery){
                 context: "#resp",
                 // url: '/lurkemurk/catalog/productsofcategory?ids=12'
                 // url: '/lurkemurk/catalog/productsofcategory?ids=12,13'
-                // url: '/lurkemurk/catalog/categories',
+                // url: '/lurkemurk/catalog/categories'
+                // url: 'lurkemurk/catalog/productsbyid?ids=38'
+                // url: 'lurkemurk/catalog/productsbyid?ids=38,142'
+                // url: 'lurkemurk/catalog/productsbyid?sku=M8-320'
                 url: '/lurkemurk/catalog/products',
                 type: 'get'
             }).done(function () {
